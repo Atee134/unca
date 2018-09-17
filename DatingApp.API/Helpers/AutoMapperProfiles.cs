5f8d38fce5c0bbfa80837ca7d74ac9ad874dcf56
Atee134 <attila.lorincz@blackbelt.hu>
2018-09-17 13:32:46 +0200

    end of section 10

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -31,6 +31,7 @@ namespace DatingApp.API.Helpers
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotoForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
